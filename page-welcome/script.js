4a715b97d10b96d04bddc126c965ef0c0389b0f8
chloeho97 <chloe-ho@hotmail.fr>
2025-03-11 18:27:05 +0100

    button function

--- page-welcome/script.js
+++ page-welcome/script.js
@@ -1,3 +1,38 @@
+// Click sur un bouton "Book" - envoi du trajet dans la collection 'cart' 
+
+function bookButtonListener() {
+
+    allBookButton = document.querySelectorAll('.book-button');
+    
+    allBookButton.forEach(button => {
+        button.addEventListener('click',function() {
+    
+        let addTripCart = {
+            departure: this.getAttribute('data-departure'),
+            arrival: this.getAttribute('data-arrival'),
+            date: this.getAttribute('data-date'),
+            price: this.getAttribute('data-price')
+            }
+            
+        fetch('http://localhost:3000/search/addTripCart', {
+            method: 'POST',
+            headers : {'Content-Type':'application/json'},
+            body: JSON.stringify(addTripCart)
+        }).then((response) => response.json())
+        .then((data) => {
+            console.log('Added to cart:', data);;
+        })
+        .catch((err) => {
+            console.error('Error while adding to cart:', err);
+        });
+    });
+    })
+    }
+
+    
+
+// Affichage des trajets disponibles selon les critères du client
+
 document.querySelector('#btn-search').addEventListener('click', function () {
 
 const searchDeparture = document.querySelector('#searchDeparture').value
@@ -25,16 +60,24 @@ fetch('http://localhost:3000/search', {
             let addTrip ='';
 
             data["Trajets trouvés"].forEach(trip => {
-                addTrip += `
-                <div class="trip-item">
-                <p>${trip.departure} > ${trip.arrival} </p>
-                <p>${new Date(trip.date).toLocaleDateString()}</p>
-                <p>${trip.price}</p>
-                <button class="book-button"> Book </button>
-                </div>`
+                    addTrip += `
+                    <div class="trip-item">
+                        <p>${trip.departure} > ${trip.arrival}</p>
+                        <p>${new Date(trip.date).toLocaleDateString()}</p>
+                        <p>${trip.price} €</p>
+                        <button class="book-button" 
+                                data-departure="${trip.departure}" 
+                                data-arrival="${trip.arrival}" 
+                                data-date="${trip.date}" 
+                                data-price="${trip.price}">
+                            Book
+                        </button>
+                    </div>`
             })
 
             resultContainer.innerHTML += addTrip;
+
+            bookButtonListener();
 
           } 
           else {
@@ -45,19 +88,3 @@ fetch('http://localhost:3000/search', {
 
 
 
-// Click sur un bouton "Book"
-
-let allBookButton = document.querySelectorAll('.book-button');
-
-allBookButton.forEach(button => {
-    button.addEventListener('click',function() {
-        
-
-        fetch('http://localhost:3000/search', {
-            method: 'POST',
-            headers : {'Content-Type':'application/json'},
-            body: JSON.stringify(userInput)
-        })
-
-    })
-})
